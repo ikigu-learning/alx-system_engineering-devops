3d5904926e6a99ca72baeae94e65c0fb9ce7872b
George Ikigu <ikigu.geo@gmail.com>
2024-05-20 21:20:30 +0300

    solution: task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+"""
+Using what you did in the task #0, extend your Python script
+to export data in the CSV format.
+
+Requirements:
+    - Records all tasks that are owned by this employee
+    - Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+    - File name must be: USER_ID.csv
+"""
+
+import requests
+import sys
+
+
+def employee_todos_to_csv(id):
+    """
+    Gets todos belonging to employee with given id
+    """
+
+    employee_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{id}"
+    )
+
+    if employee_response.status_code != 200:
+        return
+
+    todos_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{id}/todos")
+
+    if todos_response.status_code != 200:
+        return
+
+    name = employee_response.json().get('name')
+    todos = todos_response.json()
+
+    csv_data = ""
+
+    for task in todos:
+        csv_data += "\"{}\",\"{}\",\"{}\",\"{}\"\n".format(
+            id, name, task.get('completed'), task.get('title'))
+
+    with open(f"{id}.csv", "w+") as csvfile:
+        csvfile.write(csv_data)
+
+
+if __name__ == "__main__":
+    employee_todos_to_csv(sys.argv[1])
