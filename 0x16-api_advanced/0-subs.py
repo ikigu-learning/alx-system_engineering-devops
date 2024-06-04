671d37f374e7797cb963fdb5775e4b827e04553e
George Ikigu <ikigu.geo@gmail.com>
2024-06-05 00:46:09 +0300

    iter: 3-add arbitrary headers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -35,9 +35,10 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
+    headers = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     response = get(
         'https://www.reddit.com/r/{}/about.json'.format(subreddit),
-    )
+        allow_redirects=False, headers=headers)
 
     if response.ok:
         try:
