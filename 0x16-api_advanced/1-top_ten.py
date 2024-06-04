6d58d1f9abed4bb511dfcab93a75bc69e5725f27
George Ikigu <ikigu.geo@gmail.com>
2024-06-05 01:36:25 +0300

    solution: task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+Write a function that queries the Reddit API and prints
+the titles of the first 10 hot posts listed for a given subreddit.
+
+Requirements:
+    - Prototype: def top_ten(subreddit)
+    - If not a valid subreddit, print None.
+    - Note: Invalid subreddits may return a redirect to search results.
+      Ensure that you are not following redirects.
+"""
+
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and print the titles
+    of the first 10 hot posts listed for a given subreddit
+
+    Args:
+        subreddit (str): The name of the subreddit
+
+    Return:
+        Void
+    """
+
+    if subreddit is None:
+        return print(None)
+
+    api_endpoint = 'https://reddit.com/r/{}/hot.json'.format(
+        subreddit)
+
+    response = get(api_endpoint, params={'limit': 10})
+
+    if response.ok is False:
+        return
+
+    posts = response.json()
+
+    for post in posts.get('data').get('children'):
+        print(post.get('data').get('title'))
